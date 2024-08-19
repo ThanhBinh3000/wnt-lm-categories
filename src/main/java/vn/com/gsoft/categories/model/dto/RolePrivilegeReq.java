1e3368e8e1b1b6da2cd23b057a107c6b7ba39f60
vuductinh97 <vuductinh97@gmail.com>
2024-08-20 00:36:58 +0700

    update

--- src/main/java/vn/com/gsoft/categories/model/dto/RolePrivilegeReq.java
+++ src/main/java/vn/com/gsoft/categories/model/dto/RolePrivilegeReq.java
@@ -1,8 +1,10 @@
 package vn.com.gsoft.categories.model.dto;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import vn.com.gsoft.categories.model.system.BaseRequest;
 
+@EqualsAndHashCode(callSuper = true)
 @Data
 public class RolePrivilegeReq extends BaseRequest {
 
--- src/main/java/vn/com/gsoft/categories/model/dto/RoleReq.java
+++ src/main/java/vn/com/gsoft/categories/model/dto/RoleReq.java
@@ -1,7 +1,9 @@
 package vn.com.gsoft.categories.model.dto;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import vn.com.gsoft.categories.model.system.BaseRequest;
+@EqualsAndHashCode(callSuper = true)
 @Data
 public class RoleReq extends BaseRequest {
     private String roleName;
